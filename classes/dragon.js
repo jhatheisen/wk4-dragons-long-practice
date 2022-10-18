8e7fee3a7eb8d3eef2d0c5d8ee126db52a7d58d2
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-18 14:28:27 -0700

    created dragon

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,21 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  // define instance method
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...instances) {
+    let names = [];
+    for (let instance of instances) {
+      names.push(instance.name);
+    }
+    return names;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
